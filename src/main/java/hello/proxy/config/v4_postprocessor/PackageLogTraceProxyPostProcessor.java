56e3117a85da22a445e9b2f8b3a3af4ded8ac302
chunsang.yu <chunsang.yu@musinsa.com>
2022-01-12 21:54:13 +0900

    feat: chapter7 빈후처리기 api 적용

---
+++ src/main/java/hello/proxy/config/v4_postprocessor/PackageLogTraceProxyPostProcessor.java
@@ -0,0 +1,35 @@
+package hello.proxy.config.v4_postprocessor;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.aop.Advisor;
+import org.springframework.aop.framework.ProxyFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+
+@Slf4j
+@RequiredArgsConstructor
+public class PackageLogTraceProxyPostProcessor implements BeanPostProcessor {
+
+    private final String basePackage;
+    private final Advisor advisor;
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+
+        log.info("beanName: {}, bean: {}", beanName, bean);
+
+        String packageName = bean.getClass().getPackageName();
+        if (!packageName.startsWith(basePackage)) {
+            return bean;
+        }
+
+        ProxyFactory proxyFactory = new ProxyFactory(bean);
+        proxyFactory.addAdvisor(advisor);
+
+        Object proxy = proxyFactory.getProxy();
+        log.info("proxy created. target: {}, proxy: {}", bean.getClass(), proxy.getClass());
+
+        return proxy;
+    }
+}
