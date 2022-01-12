9b0c38121562e6892d0ceb40176229f90885f834
carter (Chunsang Yu) <chunsang.yu@gmail.com>
2022-01-12 22:05:33 +0900

    feat: chapter7 빈후처리기를 이용하여 로깅 처리하기 (#5)
    * test: chapter7 빈후처리기 테스트코드 추가
    
    * feat: chapter7 빈후처리기 api 적용
    
    * feat: chapter7 스피링에서 제공하는 빈후처리기 api 적용
    
    Co-authored-by: chunsang.yu <chunsang.yu@musinsa.com>

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -1,6 +1,6 @@
 package hello.proxy;
 
-import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV2;
+import hello.proxy.config.v5_autoproxy.AutoProxyConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
@@ -11,7 +11,9 @@ import org.springframework.context.annotation.Import;
 //@Import(DynamicProxyBasicConfig.class)
 //@Import(DynamicProxyFilterConfig.class)
 //@Import(ProxyFactoryConfigV1.class)
-@Import(ProxyFactoryConfigV2.class)
+//@Import(ProxyFactoryConfigV2.class)
+//@Import(BeanPostProcessorConfig.class)
+@Import(AutoProxyConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
 
